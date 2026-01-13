6adb285a5d02a8cafd8fb77f1dbd5d0326a2370f
SungYong, Eom <mike@codesand.co.kr>
2026-01-13 15:08:00 +0900

    카카오톡 채널 링크 변경

--- kakaolink.php
+++ kakaolink.php
@@ -1,5 +1,6 @@
 <?php
 ExpertNote\Core::setSingleMode(true);
-header("Location: http://pf.kakao.com/_ugtHn/chat");
+// header("Location: http://pf.kakao.com/_ugtHn/chat");
+header("Location: http://pf.kakao.com/_FxbNxon/chat");
 exit;
 ?>
