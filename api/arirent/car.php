1c5ff5f42ab8e4d83b4b017a7d717dcefa0a0352
SungYong, Eom <mike@codesand.co.kr>
2026-01-17 18:39:34 +0900

    차량 API에 color 필드 추가 (등록/수정)

--- api/arirent/car.php
+++ api/arirent/car.php
@@ -99,6 +99,7 @@ function processPost() {
         'title' => trim($parameters['title']),
         'brand' => trim($parameters['brand'] ?? ''),
         'model' => trim($parameters['model'] ?? ''),
+        'color' => trim($parameters['color'] ?? ''),
         'featured_image' => trim($parameters['featured_image'] ?? ''),
         'monthly_price' => intval($parameters['monthly_price'] ?? 0) ?: null,
         'model_year' => trim($parameters['model_year'] ?? ''),
@@ -181,7 +182,7 @@ function processPut() {
 
     // 업데이트할 필드 수집
     $allowedFields = [
-        'dealer_idx', 'car_type', 'car_number', 'title', 'brand', 'model',
+        'dealer_idx', 'car_type', 'car_number', 'title', 'brand', 'model', 'color',
         'featured_image', 'monthly_price', 'model_year', 'model_month', 'mileage_km',
         'fuel_type', 'status', 'option_exterior', 'option_safety',
         'option_convenience', 'option_seat', 'option_etc'
