bc457cc170b593e7a52948caa4454e660a13d2bb
SungYong, EOM <bluei98@gmail.com>
2026-07-20 15:16:51 +0900

    카카오 채널 링크 변경 (_FxbNxon → _NtUfX)

--- kakaolink.php
+++ kakaolink.php
@@ -1,6 +1,6 @@
 <?php
 ExpertNote\Core::setSingleMode(true);
-// header("Location: http://pf.kakao.com/_ugtHn/chat");
-header("Location: http://pf.kakao.com/_FxbNxon/chat");
+// header("Location: http://pf.kakao.com/_FxbNxon/chat");
+header("Location: http://pf.kakao.com/_NtUfX/chat");
 exit;
 ?>
